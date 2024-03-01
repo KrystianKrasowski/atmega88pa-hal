c27041f056c31bc5731ec4b7e0f60d378678e831
krystian.krasowski <krystian.krasowski@jakdojade.pl>
2024-03-01 21:55:14 +0100

    added weak attributes to interrupt callbacks

--- src/t1int.c
+++ src/t1int.c
@@ -8,11 +8,11 @@ void hal_t1int_configure(const hal_t1int_cfg_t *cfg)
   sei();
 }
 
-void hal_t1int_on_input_capture(const uint16_t value) {}
+__attribute__((weak)) void hal_t1int_on_input_capture(const uint16_t value) {}
 
-void hal_t1int_on_output_compare_a(void) {}
+__attribute__((weak)) void hal_t1int_on_output_compare_a(void) {}
 
-void hal_t1int_on_timer_overflow(void) {}
+__attribute__((weak)) void hal_t1int_on_timer_overflow(void) {}
 
 ISR(TIMER1_CAPT_vect) { hal_t1int_on_input_capture(ICR1); }
 ISR(TIMER1_COMPA_vect) { hal_t1int_on_output_compare_a(); }
