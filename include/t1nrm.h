6a23e703797715511a7bfa42a807d9e4738c7593
krystian.krasowski <krystian.krasowski@jakdojade.pl>
2024-03-01 19:11:11 +0100

    timer prescaller type extraction

--- include/t1nrm.h
+++ include/t1nrm.h
@@ -1,19 +1,10 @@
 #ifndef _LIBAVRHAL_T1NRM
 #define _LIBAVRHAL_T1NRM
 
+#include "std.h"
 #include <stdint.h>
 
-typedef enum
-{
-  HAL_T1NRM_PRESC_NONE,
-  HAL_T1NRM_PRESC_1,
-  HAL_T1NRM_PRESC_8,
-  HAL_T1NRM_PRESC_64,
-  HAL_T1NRM_PRESC_256,
-  HAL_T1NRM_PRESC_1024,
-} hal_t1nrm_prescaller_t;
-
-void hal_t1nrm_run(hal_t1nrm_prescaller_t presc);
+void hal_t1nrm_run(hal_timer_prescaller_t presc);
 void hal_t1nrm_set(uint16_t value);
 void hal_t1nrm_stop(void);
 
