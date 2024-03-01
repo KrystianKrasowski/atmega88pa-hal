4fcc4f440b15bf568b8c74fb64fcbb05df2fd980
krystian.krasowski <krystian.krasowski@jakdojade.pl>
2024-03-01 22:07:46 +0100

    add missing function declaration

--- include/t1int.h
+++ include/t1int.h
@@ -15,6 +15,7 @@ typedef struct
 
 void hal_t1int_configure(const hal_t1int_cfg_t *cfg);
 __attribute__((weak)) void hal_t1int_on_input_capture(const uint16_t value);
+__attribute__((weak)) void hal_t1int_on_output_compare_a(void);
 __attribute__((weak)) void hal_t1int_on_timer_overflow(void);
 
 #endif
