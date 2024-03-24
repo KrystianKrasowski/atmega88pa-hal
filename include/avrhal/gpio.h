91a26a78f1404cbbddeeb0f0d6ac1da0b7ca6c1e
krystian.krasowski <krystian.krasowski@jakdojade.pl>
2024-03-24 12:37:37 +0100

    add docs target with doxygen; change some gpio semantics

--- include/avrhal/gpio.h
+++ include/avrhal/gpio.h
@@ -8,21 +8,21 @@ typedef enum
     HAL_GPIO_INPUT,
     HAL_GPIO_INPUT_WITH_PULLUP,
     HAL_GPIO_OUTPUT
-} hal_gpio_dir_t;
+} hal_gpio_direction_t;
 
 typedef enum
 {
-    HAL_GPIO_REGB,
-    HAL_GPIO_REGC,
-    HAL_GPIO_REGD
-} hal_gpio_reg_t;
+    HAL_GPIO_REGISTER_B,
+    HAL_GPIO_REGISTER_C,
+    HAL_GPIO_REGISTER_D
+} hal_gpio_register_t;
 
 typedef struct
 {
-    hal_gpio_reg_t reg;
-    hal_gpio_dir_t dir;
-    uint8_t        pin;
-} hal_gpio_def_t;
+    hal_gpio_register_t  gpio_register;
+    hal_gpio_direction_t direction;
+    uint8_t              pin;
+} hal_gpio_t;
 
 typedef enum
 {
@@ -30,22 +30,57 @@ typedef enum
     HAL_GPIO_LOW
 } hal_gpio_state_t;
 
+/**
+ * @brief Initializes given GPIO definition
+ *
+ * @param[in] gpio A pioner to the GPIO definition
+ */
 void
-hal_gpio_init(const hal_gpio_def_t *def);
+hal_gpio_init(const hal_gpio_t *gpio);
 
+/**
+ * @brief Initializes whole AVR GPIO register at once with a given mask
+ *
+ * @param[in] gpio_register AVR GPIO register
+ * @param[in] mask 8-bit mask to write
+ */
 void
-hal_gpio_init_mask(const hal_gpio_reg_t reg, const uint8_t mask);
+hal_gpio_init_mask(const hal_gpio_register_t gpio_register, const uint8_t mask);
 
+/**
+ * @brief Toggles the given GPIO state
+ * 
+ * @param[in] gpio A pionter to the GPIO definition
+ */
 void
-hal_gpio_toggle(const hal_gpio_def_t *def);
+hal_gpio_toggle(const hal_gpio_t *gpio);
 
+/**
+ * @brief Writes new state to the given GPIO
+ * 
+ * @param[in] gpio A pionter to the GPIO definition
+ * @param[in] state A new state to be set on GPIO
+ */
 void
-hal_gpio_write(const hal_gpio_def_t *def, const hal_gpio_state_t state);
+hal_gpio_write(const hal_gpio_t *gpio, const hal_gpio_state_t state);
 
+/**
+ * @brief Writes whole AVR GPIO register at onve with a given mask
+ * 
+ * @param[in] gpio_register AVR GPIO register
+ * @param[in] mask 8-bit mask to write
+ */
 void
-hal_gpio_write_mask(const hal_gpio_reg_t reg, const uint8_t mask);
+hal_gpio_write_mask(const hal_gpio_register_t gpio_register,
+                    const uint8_t             mask);
 
+/**
+ * @brief Reads the state of a given GPIO
+ * 
+ * @param[in] gpio 
+ * @return hal_gpio_state_t 
+ */
 hal_gpio_state_t
-hal_gpio_read(const hal_gpio_def_t *def);
+hal_gpio_read(const hal_gpio_t *gpio);
 
 #endif
--- src/avrhal/gpio.c
+++ src/avrhal/gpio.c
@@ -2,76 +2,76 @@
 #include <avr/io.h>
 
 static volatile uint8_t * const DDRx[] = {
-    [HAL_GPIO_REGB] = &DDRB,
-    [HAL_GPIO_REGC] = &DDRC,
-    [HAL_GPIO_REGD] = &DDRD,
+    [HAL_GPIO_REGISTER_B] = &DDRB,
+    [HAL_GPIO_REGISTER_C] = &DDRC,
+    [HAL_GPIO_REGISTER_D] = &DDRD,
 };
 
-volatile uint8_t * const PORTx[] = {
-    [HAL_GPIO_REGB] = &PORTB,
-    [HAL_GPIO_REGC] = &PORTC,
-    [HAL_GPIO_REGD] = &PORTD,
+static volatile uint8_t * const PORTx[] = {
+    [HAL_GPIO_REGISTER_B] = &PORTB,
+    [HAL_GPIO_REGISTER_C] = &PORTC,
+    [HAL_GPIO_REGISTER_D] = &PORTD,
 };
 
-volatile uint8_t * const PINx[] = {
-    [HAL_GPIO_REGB] = &PINB,
-    [HAL_GPIO_REGC] = &PINC,
-    [HAL_GPIO_REGD] = &PIND,
+static volatile uint8_t * const PINx[] = {
+    [HAL_GPIO_REGISTER_B] = &PINB,
+    [HAL_GPIO_REGISTER_C] = &PINC,
+    [HAL_GPIO_REGISTER_D] = &PIND,
 };
 
 void
-hal_gpio_init(const hal_gpio_def_t *def)
+hal_gpio_init(const hal_gpio_t *def)
 {
-    switch (def->dir)
+    switch (def->direction)
     {
         case HAL_GPIO_INPUT:
-            *(DDRx[def->reg]) &= ~(1 << def->pin);
+            *(DDRx[def->gpio_register]) &= ~(1 << def->pin);
             break;
         case HAL_GPIO_INPUT_WITH_PULLUP:
-            *(DDRx[def->reg]) &= ~(1 << def->pin);
-            *(PORTx[def->reg]) |= (1 << def->pin);
+            *(DDRx[def->gpio_register]) &= ~(1 << def->pin);
+            *(PORTx[def->gpio_register]) |= (1 << def->pin);
             break;
         case HAL_GPIO_OUTPUT:
-            *(DDRx[def->reg]) |= (1 << def->pin);
+            *(DDRx[def->gpio_register]) |= (1 << def->pin);
             break;
     }
 }
 
 void
-hal_gpio_init_mask(const hal_gpio_reg_t reg, const uint8_t mask)
+hal_gpio_init_mask(const hal_gpio_register_t gpio_register, const uint8_t mask)
 {
-    *(DDRx[reg]) = mask;
+    *(DDRx[gpio_register]) = mask;
 }
 
 void
-hal_gpio_toggle(const hal_gpio_def_t *def)
+hal_gpio_toggle(const hal_gpio_t *def)
 {
-    *(PORTx[def->reg]) ^= (1 << def->pin);
+    *(PORTx[def->gpio_register]) ^= (1 << def->pin);
 }
 
 void
-hal_gpio_write(const hal_gpio_def_t *def, const hal_gpio_state_t state)
+hal_gpio_write(const hal_gpio_t *def, const hal_gpio_state_t state)
 {
     if (state == HAL_GPIO_HIGH)
     {
-        *(PORTx[def->reg]) |= (1 << def->pin);
+        *(PORTx[def->gpio_register]) |= (1 << def->pin);
     }
     else
     {
-        *(PORTx[def->reg]) &= ~(1 << def->pin);
+        *(PORTx[def->gpio_register]) &= ~(1 << def->pin);
     }
 }
 
 void
-hal_gpio_write_mask(const hal_gpio_reg_t reg, const uint8_t mask)
+hal_gpio_write_mask(const hal_gpio_register_t gpio_register, const uint8_t mask)
 {
-    *(PORTx[reg]) = mask;
+    *(PORTx[gpio_register]) = mask;
 }
 
 hal_gpio_state_t
-hal_gpio_read(const hal_gpio_def_t *def)
+hal_gpio_read(const hal_gpio_t *def)
 {
-    if (*(PINx[def->reg]) & (1 << def->pin))
+    if (*(PINx[def->gpio_register]) & (1 << def->pin))
     {
         return HAL_GPIO_HIGH;
     }
